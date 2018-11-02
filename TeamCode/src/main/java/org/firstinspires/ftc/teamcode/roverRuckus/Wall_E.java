5b2980822cf632f2bf21154ccb63ba2ccee83c71
Eric <3rix40@gmail.com>
2018-11-01 19:22:40 -0500

    page 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roverRuckus/Wall_E.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roverRuckus/Wall_E.java
@@ -2,11 +2,12 @@ package org.firstinspires.ftc.teamcode.roverRuckus;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
+//page 2
+
 @Autonomous (name = "Wall-E", group = "Auto")
 public class Wall_E extends OmniAutoMode{
     //
     public void runOpMode(){
-        //
         //
         //
         //
