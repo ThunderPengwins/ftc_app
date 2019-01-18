bdb9c33a108f72cd8f2d02c29c22cdfa84681d51
Eric <3rix40@gmail.com>
2019-01-18 09:26:50 -0600

    Potentiometer stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roverRuckus/ArmyTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roverRuckus/ArmyTest.java
@@ -1,71 +1,73 @@
 package org.firstinspires.ftc.teamcode.roverRuckus;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.DcMotor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @TeleOp(name = "ArmyTest", group = "test")
 public class ArmyTest extends OmniMode {
     //
-    DcMotor soldier;
+    static final double pr1pow = .2;
+    static final double liepow = .2;
+    //
+    static final double pr1vangle = 1.0;
+    static final double lievangle = 1.0;
+    //
+    static final List<Double> voltage = new ArrayList<Double>(){{
+        add(.002);
+        add(.139);
+        add(.279);
+        add(.401);
+        add(.515);
+        add(.643);
+        add(.743);
+        add(.875);
+        add(1.006);
+        add(1.125);
+        add(1.302);
+        add(1.512);
+        add(1.616);
+        add(1.87);
+        add(2.118);
+        add(2.423);
+        add(2.794);
+        add(3.311);
+        add(3.334);
+    }};
+    //
+    DcMotor pr1vate;
     DcMotor lieutenant;
     //
-    //closed = 1
-    //open = 0.3
-    //
-    static final Double closed = .5;
-    static final Double open = 0.1;
+    AnalogInput cinput;
+    AnalogInput dinput;
     //
     public void runOpMode() {
         //
         //<editor-fold desc="HardwareMap">
-        soldier = hardwareMap.dcMotor.get("soldier");//change in phones
+        pr1vate = hardwareMap.dcMotor.get("pr1vate");//change in phones
         lieutenant = hardwareMap.dcMotor.get("lieutenant");
-        //
+        cinput = hardwareMap.analogInput.get("cinput");
+        dinput = hardwareMap.analogInput.get("dinput");
         //</editor-fold>
         //
         waitForStartify();
         //
-        //<editor-fold desc="Variables">
-        Double power = 1.0;
-        Float leftC;
-        Float rightC = 0F;
-        Boolean auto = false;
-        Integer direction = 1;
-        Boolean powerP = false;
-        Double position = 0.5;
-        Integer movement = 0;
-        //</editor-fold>
-        //
         while (opModeIsActive()) {
             //
-            leftC = -gamepad2.left_stick_y;//left y
-            rightC = -gamepad2.right_stick_x;//right x
-            //
             //<editor-fold desc="Set auto move">
-            if (gamepad2.y) {
-                direction = 1;
-                if (!auto) {
-                    auto = true;
-                }
-            } else if (gamepad2.x) {
-                direction = -1;
-                if (!auto) {
-                    auto = true;
-                }
-            }
-            //</editor-fold>
-            //
-            //<editor-fold desc="set moter power">
-
-            //
-            if (gamepad2.a) {
-                position = open;
-            } else if (gamepad2.b){
-                position = closed;
-            }
-            //
+            telemetry.addData("cinput", cinput.getVoltage() + ", degrees: " + voltogres(cinput.getVoltage()));
+            telemetry.addData("dinput", dinput.getVoltage() + ", degrees: " + voltogres(dinput.getVoltage()));
             telemetry.update();
         }
     }
     //
+    public double voltogres(double volts){
+        int n;
+        for (n = 0; n < voltage.size() && voltage.get(n) < volts; n++){}
+        //
+        return (15 / (voltage.get(n) - voltage.get(n - 1))) * (volts - voltage.get(n - 1));
+    }
 }
