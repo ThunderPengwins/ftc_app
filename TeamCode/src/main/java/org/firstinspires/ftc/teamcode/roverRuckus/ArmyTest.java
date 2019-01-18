2b18dd5570f05b655bb39c595fb6e7c9b8db22b0
Eric <3rix40@gmail.com>
2019-01-18 13:50:15 -0600

    -Fixed Potentiometer -Wrote Arm Math

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roverRuckus/ArmyTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roverRuckus/ArmyTest.java
@@ -4,11 +4,16 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.AnalogInput;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
 
 @TeleOp(name = "ArmyTest", group = "test")
 public class ArmyTest extends OmniMode {
+    //
+    static final double pr1len = 14.5;
+    static final double lielen = 20;
     //
     static final double pr1pow = .2;
     static final double liepow = .2;
@@ -17,7 +22,7 @@ public class ArmyTest extends OmniMode {
     static final double lievangle = 1.0;
     //
     static final List<Double> voltage = new ArrayList<Double>(){{
-        add(.002);
+        add(-.002);
         add(.139);
         add(.279);
         add(.401);
@@ -53,13 +58,29 @@ public class ArmyTest extends OmniMode {
         dinput = hardwareMap.analogInput.get("dinput");
         //</editor-fold>
         //
+        double c;
+        double d;
+        //
+        List<Double> cords;
+        List<Double> powers;
+        //
         waitForStartify();
         //
         while (opModeIsActive()) {
             //
+            c = voltogres(cinput.getVoltage());
+            d = voltogres(dinput.getVoltage());
+            //
+            cords = xny(c, d);
+            //
+            powers = lumina(cords.get(0), cords.get(1), c, d);
+            //
             //<editor-fold desc="Set auto move">
-            telemetry.addData("cinput", cinput.getVoltage() + ", degrees: " + voltogres(cinput.getVoltage()));
-            telemetry.addData("dinput", dinput.getVoltage() + ", degrees: " + voltogres(dinput.getVoltage()));
+            telemetry.addData("cinput", round(cinput.getVoltage(), 2) + ", degrees: " + round(voltogres(cinput.getVoltage()), 2));
+            telemetry.addData("dinput", round(dinput.getVoltage(), 2) + ", degrees: " + round(voltogres(dinput.getVoltage()), 2));
+            telemetry.addData("x", round(cords.get(0), 2) + ", y: " + round(cords.get(1), 2));
+            telemetry.addData("private's power", round(powers.get(0), 2));
+            telemetry.addData("lieutenant's power", round(powers.get(1), 2));
             telemetry.update();
         }
     }
@@ -68,6 +89,82 @@ public class ArmyTest extends OmniMode {
         int n;
         for (n = 0; n < voltage.size() && voltage.get(n) < volts; n++){}
         //
-        return (15 / (voltage.get(n) - voltage.get(n - 1))) * (volts - voltage.get(n - 1));
+        return ((n * 15) / (voltage.get(n))) * (volts);
+    }
+    //
+    public static List<Double> xny(double c, double da) {
+        //B1*COS(RADIANS(G6))+B2*COS(RADIANS(H7-(90-G6)))
+        //B1*SIN(RADIANS(G6))+B2*SIN(RADIANS(H7-(90-G6)))
+        List<Double> xny = new ArrayList<>();
+        //IF(B4-90>-1,B4-90,B4+270)
+        double d;
+        if (da - 90 > -1){
+            d = da - 90;
+        }else{
+            d = da + 270;
+        }
+        //
+        double x = pr1len * Math.cos(Math.toRadians(c)) + lielen * Math.cos(Math.toRadians(d - (90 - c)));
+        double y = pr1len * Math.sin(Math.toRadians(c)) + lielen * Math.cos(Math.toRadians(d - (90 - c)));
+        //
+        xny.add(x);
+        xny.add(y);
+        //
+        return xny;
+    }
+    //
+    public static List<Double> lumina(double x, double y, double c, double d){
+        //DEGREES(ATAN(G4/G3)) + DEGREES(ACOS(/(G1^2+(G3^2+G4^2)-G2^2)/(2*G1*SQRT(G3^2+G4^2))/))
+        //degrees(asin(sin(acos((G1^2+G3^2+G4^2-G2^2)/(2*G1*sqrt(G3^2+G4^2))))*sqrt(G3^2+G4^2)/G2))
+        /* g1 - pr1len
+        g2 - lielen
+        g3 - x
+        g4 - y
+         */
+        List<Double> pnl = new ArrayList<>();
+        //
+        double cb = Math.toDegrees(Math.atan(y / x)) + Math.toDegrees(Math.acos((Math.pow(pr1len, 2) + (Math.pow(x, 2) + Math.pow(y, 2)) - Math.pow(lielen, 2)) / (2 * pr1len * Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)))));
+        double db = Math.toDegrees(Math.asin(Math.sin(Math.acos((Math.pow(pr1len, 2) + (Math.pow(x, 2) + Math.pow(y, 2)) - Math.pow(lielen, 2)) / (2 * pr1len * Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2))))) * Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)) / lielen));
+        //IF(G7-90>-1,G7-90,G7+270)
+        if (db - 90 > -1){
+            db = db - 90;
+        }else{
+            db = db + 270;
+        }
+        //
+        double p = cb - c;
+        double l = db - d;
+        //
+        double ps;
+        double ls;
+        //
+        if (Math.abs(p) > Math.abs(l)){
+            ps = 1.0;
+            ls = l / p;
+        } else {
+            ps = p / l;
+            ls = 1.0;
+        }
+        //
+        if (p < -1){
+            ps = ps * -1;
+        }
+        //
+        if (l < -1){
+            ls = ls * -1;
+        }
+        //
+        pnl.add(ps);
+        pnl.add(ls);
+        //
+        return pnl;
+    }
+    //
+    public static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roverRuckus/Moby.java
@@ -0,0 +1,153 @@
+package org.firstinspires.ftc.teamcode.roverRuckus;
+
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+/**
+ * @author Eric
+ */
+
+@TeleOp (name = "Moby", group = "real")
+public class Moby extends OmniMode {
+    //<editor-fold desc="hardware">
+    /**
+     * <h1>Hardware</h>
+     * <p>Here is where we create any piece of hardware that
+     * wasn't already created in the class above, OmniMode.
+     * Later on, we do the hardware map, but that can't be done
+     * until we say:</p>
+     * @code runOpMode(){}
+     */
+    ModernRoboticsI2cRangeSensor jeep;
+    DcMotor verticala;
+    DcMotor verticalb;
+    DigitalChannel down;
+    DigitalChannel up;
+    Servo latch;
+    //</editor-fold>
+    //
+    static final Double closed = 0.92;
+    static final Double open = 0.5;
+    //
+    public void runOpMode() {
+        //
+        //<editor-fold desc="Init">
+        telInit("hardware");
+        left = hardwareMap.dcMotor.get("left");
+        right = hardwareMap.dcMotor.get("right");
+        jeep = hardwareMap.get(ModernRoboticsI2cRangeSensor.class, "jeep");
+        verticala = hardwareMap.dcMotor.get("verticala");
+        verticalb = hardwareMap.dcMotor.get("verticalb");
+        down = hardwareMap.get(DigitalChannel.class, "down");
+        up = hardwareMap.get(DigitalChannel.class, "up");
+        latch = hardwareMap.servo.get("latch");
+        //
+        down.setMode(DigitalChannel.Mode.INPUT);
+        up.setMode(DigitalChannel.Mode.INPUT);
+        //
+        verticala.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        verticalb.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        verticala.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        verticalb.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        verticalb.setDirection(DcMotorSimple.Direction.REVERSE);
+        //</editor-fold>
+        //
+        waitForStartify();
+        //
+        //<editor-fold desc="Variables">
+        Integer direction = 0;//lifter direction
+        Double rightPower = .5;//right motor power
+        Double leftPower = .5;//left motor power
+        Integer front = 1;//front side of robot
+        Integer ready = 1;
+        //
+        configureMotors();
+        telInit("complete");
+        //</editor-fold>
+        //
+        waitForStartify();
+        //
+        while (opModeIsActive()) {
+            //
+            left.setPower(-gamepad1.left_stick_y * leftPower * front);
+            right.setPower(-gamepad1.right_stick_y * rightPower * front);
+            //
+            //<editor-fold desc="Power Levels">
+            if (gamepad1.right_trigger > 0) {
+                rightPower = .2;
+                telemetry.addData("Trigger", "Right");
+            } else if (gamepad1.right_bumper) {
+                rightPower = .05;
+                telemetry.addData("Bumper", "Right");
+            } else {
+                rightPower = .5;
+                telemetry.addData("No action", "Right");
+            }
+            //
+            if (gamepad1.left_trigger > 0) {
+                leftPower = .2;
+                telemetry.addData("Trigger", "Left");
+            } else if (gamepad1.left_bumper) {
+                leftPower = .05;
+                telemetry.addData("Bumper", "Left");
+            } else {
+                leftPower = .5;
+                telemetry.addData("No action", "Left");
+            }
+            //</editor-fold>
+            //
+            //<editor-fold desc="Lifter">
+            if (!((up.getState() && direction == 1) || (down.getState() && direction == -1)) && !(gamepad2.right_trigger > 0) && !(direction == 0)) {
+                verticala.setPower(0.5 * direction);
+                verticalb.setPower(0.5 * direction);
+            } else {
+                verticala.setPower(0);
+                verticalb.setPower(0);
+                direction = 0;
+            }
+            //
+            if (gamepad2.y){
+                direction = 1;
+            } else if (gamepad2.x){
+                direction = -1;
+            }
+            //</editor-fold>
+            //
+            //<editor-fold desc="Latch">
+            if (gamepad2.a) {
+                latch.setPosition(open);
+            } else if (gamepad2.b) {
+                latch.setPosition(closed);
+            }
+            //</editor-fold>
+            //
+            //<editor-fold desc="Front">
+            if (gamepad1.b) {
+                if (front == 1 && ready == 1) {
+                    front = -1;
+                } else if (front == -1 && ready == 1) {
+                    front = 1;
+                }
+                ready = 0;
+            } else {
+                ready = 1;
+            }
+            //</editor-fold>
+            //
+            //<editor-fold desc="Telemetry">
+            telemetry.addData("ready?", ready);
+            telemetry.addData("front", front);
+            telemetry.addData("distance", jeep.getDistance(DistanceUnit.INCH));
+            telemetry.addData("up", up.getState());
+            telemetry.addData("down", down.getState());
+            telemetry.update();
+            //</editor-fold>
+        }
+    }
+}
